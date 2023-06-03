304d10c2d82a33cfea20c4528446d03f03ece592
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-06-03 17:02:06 +0530

    fix: scroll

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,13 +3,13 @@ const Contact = () => {
   return (
     <div className="contact">
       <h4>
-        Let's create something together <span>EMAIL ME</span>
+        ⚠️ Work in Progress <span>EMAIL ME</span>
       </h4>
       <h4>
-        Let's create something together <span>EMAIL ME</span>
+        ⚠️ Work in Progress <span>EMAIL ME</span>
       </h4>
       <h4>
-        Let's create something together <span>EMAIL ME</span>
+        ⚠️ Work in Progress <span>EMAIL ME</span>
       </h4>
     </div>
   );
